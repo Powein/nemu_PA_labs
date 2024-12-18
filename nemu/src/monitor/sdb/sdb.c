8b09b7cdb7944c9c157433c646ea775e5bc5d12f
tracer-ics2024 <tracer@njuics.org>
2024-12-18 20:52:41 +0800

    >  compile NEMU 231220000 张三 Linux Unix 6.11.0-9-generic #9-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct 14 13:19:59 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:52:41 up 1 day, 22:51,  2 users,  load average: 0.55, 0.80, 0.80

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -124,10 +124,10 @@ static int cmd_x(char *args) {
   };
   word_t byte_count = atoi(arg_bc);
   word_t address = strtol(arg_ad, NULL, 16);
-  Log("Get the args");
+  // Log("Get the args");
   for (int i = 0; i < byte_count; i++) {
-    // word_t t = paddr_read(address + i, 1);
-    printf("%x", address + i);
+    word_t t = paddr_read(address + i, 1);
+    printf("%x %x", address + i, t);
   }
   return 0;
 }

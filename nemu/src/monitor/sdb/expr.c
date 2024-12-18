a0bf1bc43939c7d9186825a6454082922c2e0cb3
tracer-ics2024 <tracer@njuics.org>
2024-12-18 21:43:06 +0800

    >  compile NEMU 231220000 张三 Linux Unix 6.11.0-9-generic #9-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct 14 13:19:59 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:43:06 up 1 day, 23:42,  2 users,  load average: 1.56, 1.09, 1.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -122,7 +122,7 @@ static struct rule {
   {"[0-9]+", TK_DECIMAL}, 
   {"\\(", TK_LEFT_P},// (
   {"\\)", TK_RIGHT_P},// )
-  {"\\$[a-z]+", TK_REGISTER},
+  {"\\$(\\$0|ra|sp|gp|tp|t0|t1|t2|s0|s1|a0|a1|a2|a3|a4|a5|a6|a7|s2|s3|s4|s5|s6|s7|s8|s9|s10|s11|t3|t4|t5|t6)", TK_REGISTER},
 };
 // len(rules)
 #define NR_REGEX ARRLEN(rules)

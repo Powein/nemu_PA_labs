1ad5efa608e792a6704fad28fe49c9bbebe91bb4
tracer-ics2024 <tracer@njuics.org>
2024-12-19 00:23:46 +0800

    >  compile NEMU 231220000 张三 Linux Unix 6.11.0-9-generic #9-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct 14 13:19:59 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:23:46 up 2 days,  2:23,  2 users,  load average: 0.35, 0.87, 0.95

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -108,7 +108,7 @@ static int cmd_x(char *args) {
   if(!*success) {
     printf("Invalid expression for byte count");
   }
-  Log("The address is %s", arg_ad);
+  // Log("The address is %s", arg_ad);
   // word_t address = expr(arg_ad, success);
   word_t address = strtol(arg_ad, NULL, 16);
   // if(!*success) {

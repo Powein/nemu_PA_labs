b59995f2dc889e234a48f126e79813a34824d0ee
tracer-ics2024 <tracer@njuics.org>
2024-12-19 18:01:32 +0800

    >  compile NEMU 231220000 张三 Linux Unix 6.11.0-9-generic #9-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct 14 13:19:59 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:01:32 up 2 days, 20:00,  2 users,  load average: 1.40, 1.84, 2.23

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -38,6 +38,7 @@ void init_wp_pool() {
   for (i = 1; i < NR_WP; i ++) { 
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    wp_pool[i].vacant = true;
   }
   free_ = wp_pool; // set free to the first element
   Log("Watchpoint pool initialized. free_ = %p, head = %p", free_, head);

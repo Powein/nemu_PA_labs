c0e9ed74cb0b0f0d001af0522da6cc9fbd1e07c7
tracer-ics2024 <tracer@njuics.org>
2025-01-01 22:30:21 +0800

    >  compile NEMU 231220000 张三 Linux Unix 6.11.0-13-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov 30 23:51:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:30:21 up  8:01,  2 users,  load average: 0.63, 1.05, 1.13

--- nemu/src/engine/interpreter/mytest.c
+++ nemu/src/engine/interpreter/mytest.c
@@ -5,6 +5,8 @@ word_t vaddr_read(vaddr_t addr, int len);
 void vaddr_write(vaddr_t addr, int len, word_t data);
 void mytest() {
     vaddr_write(0x80000000, 1, 0xDEADBEEF);
+    vaddr_write(0x80000010, 1, 0xDEADBEEF);
+    vaddr_write(0x80000020, 1, 0xDEADBEEF);
     // vaddr_write(0x80000004, 1, 0xDEADBEEF);
     word_t a = vaddr_ifetch(0x80000000, 4);
     Warn("mytest: 0x%08x\n", a);
